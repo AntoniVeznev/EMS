18738eb03d791bd0e2921510bde340ad6a97e175
Antoni Veznev <antoniveznev@gmail.com>
2023-08-28 10:35:20 +0300

    Adding ModelMapper dependency and making configuration for Spring Security and ModelMapper.

---
+++ EMPLOYEE MANAGEMENT SYSTEM/src/main/java/com/example/employee_management_system/service/impl/ApplicationUserDetailServiceImpl.java
@@ -0,0 +1,49 @@
+package com.example.employee_management_system.service.impl;
+
+
+import com.example.employee_management_system.model.entity.Role;
+import com.example.employee_management_system.model.entity.User;
+import com.example.employee_management_system.repository.UserRepository;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ApplicationUserDetailServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public ApplicationUserDetailServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository
+                .findUserByUsername(username)
+                .map(this::map)
+                .orElseThrow(() -> new UsernameNotFoundException("User with name " + username + " not found!"));
+    }
+
+    private UserDetails map(User user) {
+        return new org.springframework.security.core.userdetails.User(
+                user.getUsername(),
+                user.getPassword(),
+                extractAuthorities(user)
+        );
+    }
+
+    private List<GrantedAuthority> extractAuthorities(User user) {
+        List<GrantedAuthority> authorityList = new ArrayList<>();
+        authorityList.add(mapRole(user.getRole()));
+        return authorityList;
+    }
+
+    private GrantedAuthority mapRole(Role role) {
+        return new SimpleGrantedAuthority("ROLE_" + role.getRole().name());
+    }
+}
